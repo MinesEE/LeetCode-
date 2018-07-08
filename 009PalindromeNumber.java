237def18c0b5412a24709a43b2df773ed747c8c1
Blaster <Dehui@Dehuis-MacBook-Pro.local>
2018-07-08 17:49:13 -0600

    first commit for 009

---
+++ 009PalindromeNumber.java
@@ -0,0 +1,46 @@
+/**
+Determine whether an integer is a palindrome. 
+An integer is a palindrome when it reads the same backward as forward.
+
+Example 1:
+Input: 121
+Output: true
+
+Example 2:
+Input: -121
+Output: false
+Explanation: From left to right, it reads -121. 
+From right to left, it becomes 121-. Therefore it is not a palindrome.
+
+Example 3:
+Input: 10
+Output: false
+Explanation: Reads 01 from right to left. 
+Therefore it is not a palindrome.
+
+Follow up:
+Coud you solve it without converting the integer to a string?
+*/
+
+
+/**
+Idea: 
+1. Consider the corner case: 0, 10, 100, 1000 
+2. Apparently, single digit like 7 works
+*/
+class 009PalindromeNumber {
+    public static boolean isPalindrome(int x) {
+        if (x < 0) return false;
+        if (x > 0 && x % 10 == 0) return false;
+        int sum = 0;
+        while (sum < x) {
+            sum = 10 * sum + x % 10;
+            x = x / 10;
+        }
+        return (sum == x || sum / 10 == x);
+    }
+    public static void main(String[] args) {
+        int x = 12321;
+        System.out.println(isPalindrome(x));
+    }
+}
