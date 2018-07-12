9e730e2096fafc0e1506dff469edd0f12e3db4b3
Blaster <Dehui@Dehuis-MacBook-Pro.local>
2018-07-11 19:56:53 -0600

    first commit for 018

---
+++ 0184Sum.java
@@ -0,0 +1,64 @@
+/**
+Given an array nums of n integers and an integer target, 
+are there elements a, b, c, and d in nums such that a + b + c + d = target? 
+Find all unique quadruplets in the array which gives the sum of target.
+
+The solution set must not contain duplicate quadruplets.
+
+Example:
+
+Given array nums = [1, 0, -1, 0, -2, 2], and target = 0.
+
+A solution set is:
+[
+  [-1,  0, 0, 1],
+  [-2, -1, 1, 2],
+  [-2,  0, 0, 2]
+]
+*/
+class 0184Sum {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+    List<List<Integer>> list = new ArrayList<List<Integer>>();
+    if (nums == null || nums.length < 4) {
+        return list;
+    }
+    Arrays.sort(nums);
+    for (int i = 0; i < nums.length - 3; i++) {
+        for (int j = i + 1; j < nums.length - 2; j++) {
+            int left = j + 1;
+            int right = nums.length - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[j] + nums[left] + nums[right];
+                if (sum == target) {
+                    list.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    right--;
+                    left++;
+                } else if (sum > target) {
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+                    right--;
+                } else {
+                    while (left < right && nums[left] == nums[left + 1]) {  
+                        left++;
+                    }
+                    left++;
+                }
+            }
+            while (j < nums.length - 2 && nums[j] == nums[j + 1]) {
+                j++;
+            }
+        }
+        while (i < nums.length - 3 && nums[i] == nums[i + 1]) {
+            i++;
+        }
+    }
+    return list;
+    }
+}
