e48d0762df2c7686e49c037e81cd96b4d6a5cdce
Blaster <Dehui@Dehuis-MacBook-Pro.local>
2019-05-03 16:52:51 -0600

    LeetCode 100, Same Tree

---
+++ Tree100.java
@@ -0,0 +1,21 @@
+// 100. Same Tree
+// Given two binary trees, write a function to check if they are the same or not.
+// 
+// Two binary trees are considered the same if they are structurally 
+// identical and the nodes have the same value.
+// 
+// Example 1:
+// 
+// Input:     1         1
+//           / \       / \
+//          2   3     2   3
+// 
+//         [1,2,3],   [1,2,3]
+// 
+// Output: true
+
+class Tree100 {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        
+    }
+}
