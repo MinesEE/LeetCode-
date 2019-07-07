a171036200b4bcb75400c600d824b38af627d2ea
Blaster <Dehui@Dehuis-MacBook-Pro.local>
2019-07-07 15:32:20 -0400

    169 Majority Element added majority voting algorithm

--- Array169.java
+++ Array169.java
@@ -11,6 +11,33 @@
 // 
 // Input: [2,2,1,1,1,2,2]
 // Output: 2
+
+// Majority Voting Algorithm 
+class Solution {
+    public int majorityElement(int[] nums) {
+        if (nums.length == 1) return nums[0];
+        
+        int count = 1;
+        int candidate = nums[0];
+        
+        for (int i = 1; i < nums.length; i++) {
+            if (count == 0) {
+                count++;
+                candidate = nums[i];
+            } else {
+                if (candidate != nums[i]) {
+                    count--;
+                } else {
+                    count++;
+                }
+            }
+        }
+        return candidate;
+    }
+}
+
+
+// Hashmap solution容易理解，但是空间复杂度太高了
 class Array169 {
     public int majorityElement(int[] nums) {
         HashMap<Integer, Integer> hash = new HashMap<Integer, Integer>();
